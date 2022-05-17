0845923a3bd4eb2351f13c54d5538802fa877b56
mehmeturmac <mehmeturmac@gmail.com>
2022-05-17 16:15:35 +0300

    React first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # React-Odev1
+### Bu ödev aşağıdaki konuları içermektedir.
+- Callback Functions / async-await
+- ES6 sistemiile import işlemi
+- Axios module kullanımı
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+[Patika](https://www.patika.dev)
---
+++ app.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default async (number) => {
+  const { data: user } = await axios(
+    "https://jsonplaceholder.typicode.com/users/" + number
+  );
+  const { data: posts } = await axios(
+    "https://jsonplaceholder.typicode.com/posts?userId=" + number
+  );
+
+  return { user, posts };
+};
